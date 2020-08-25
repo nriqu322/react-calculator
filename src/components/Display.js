74910ec6f94c2bbf7e84cc86283eb2993d438932
Luis Saavedra <nriqu322@gmail.com>
2020-08-25 10:58:03 -0600

    implement button component

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
-  <div>
-    {result}
+  <div className="result">
+    <p>{result}</p>
   </div>
 );
 
@@ -14,3 +14,5 @@ Display.defaultProps = {
 Display.propTypes = {
   result: PropTypes.string,
 };
+
+export default Display;
