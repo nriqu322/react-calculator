9e8f8b05ad38f284135fb2b84a6e6d909038070c
Luis Saavedra <nriqu322@gmail.com>
2020-08-26 10:20:54 -0600

    add calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,1 +1,22 @@
 import Big from './big.mjs';
+
+const calculate = (dataObj, btnName) => {
+  let { total, next, operation } = dataObj;
+
+  if (btnName === '+/-') {
+    total *= -1;
+    next *= -1;
+  } else if (btnName === 'AC') {
+    total = 0;
+    next = null;
+    operation = null;
+  } else {
+    total = null;
+    next = null;
+    operation = null;
+  }
+
+  return (total, next, operation);
+};
+
+export default calculate;
